7750dad2938f43f530c10e5bd499eed9ea09e6ed
Jason Song <i@wolfogre.com>
2017-12-08 09:53:05 +0800

    (0.3.4) update count-conn

--- func/count-conn.sh
+++ func/count-conn.sh
@@ -6,5 +6,6 @@ if [ -z $PORT ]; then
 	read -p "which port?" PORT
 fi
 
-lsof -i:$PORT | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort  | uniq -c | sort -n -k 1 -r
+#lsof -i:$PORT | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort  | uniq -c | sort -n -k 1 -r
+netstat -nat | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}:${PORT}\b.*([0-9]{1,3}\.){3}[0-9]{1,3}" | awk '{print $2}' | sort | uniq -c | sort -n -r -k 1
 
